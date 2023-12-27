ccd0db0d7b0f437621f95bdd773f5da442c6dbbd
Syed Easin <syed@syed.link3.net>
2023-12-27 15:19:19 +0600

    Factorial Calculation

--- src/FactorialCalculationReverse.java
+++ src/FactorialCalculationReverse.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class FactorialCalculation {
+public class FactorialCalculationReverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         System.out.println(factorialCalc(num));
     }
     public static long factorialCalc(int num) {
-        int totalValue = 1;
-        for(int i = 1; i <= num; i++) {
-            totalValue = totalValue*i;
+        long totalValue = 1l;
+        for (int i =0; i < num; i++) {
+            totalValue = totalValue*(num-i);
         }
         return totalValue;
     }
